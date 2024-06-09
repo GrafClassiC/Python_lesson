d0976e3d5c5a0b5ac9b56b27952d3769169a9ebc
Artem <vlasov.art.ser@yandex.ru>
2024-06-09 12:30:55 +0300

    добавил seminar7

--- seminar6.py
+++ seminar6.py
@@ -96,24 +96,24 @@
 220 284
 """
 
-def get_sum(n):
-    my_sum = 1
-    for el in range(2, n):
-        if n % el == 0:
-            my_sum += el
-    return my_sum
+# def get_sum(n):
+#     my_sum = 1
+#     for el in range(2, n):
+#         if n % el == 0:
+#             my_sum += el
+#     return my_sum
 
-def get_friendlies(k):
-    lst = []
-    for n in range (1, k + 1):
-        if n not in lst:
-            m = get_sum(n)
-            if n == get_sum(m) and n != m:
-                lst.append(n)
-                lst.append(m)
-    return lst
+# def get_friendlies(k):
+#     lst = []
+#     for n in range (1, k + 1):
+#         if n not in lst:
+#             m = get_sum(n)
+#             if n == get_sum(m) and n != m:
+#                 lst.append(n)
+#                 lst.append(m)
+#     return lst
 
-print(get_friendlies(2000))
+# print(get_friendlies(2000))
 
 
 """
